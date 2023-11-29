04340c24d77b8e756fc7d980805b1f3012f34a50
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2023-11-28 22:41:29 -0300

    Remove arquivos ignorados

--- render.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-plantuml *.puml
-
-find *.Rmd | while read file; do
-  Rscript -e "rmarkdown::render('$file', output_format = 'all')"
-done
--- rpubsUpload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-Rscript -e "rsconnect::rpubsUpload('Instalação do React Native', 'Tutorial-de-instalação-Ract-Native.html')"
